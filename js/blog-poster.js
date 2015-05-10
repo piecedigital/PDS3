304b25557eccae7b8ecc66202455a76c3c1ce592
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-05-09 23:33:27 -0400

    finished blog and added disqus. now only need to optimize images and finish testing

--- js/blog-poster.js
+++ js/blog-poster.js
@@ -10,18 +10,27 @@ $(document).ready(function(){
       //blogs json load successful, proceed
       var post = window.location.search.split("=").pop();
       post -= 1;
-      console.log("blogs: " + blogs);
-      console.log(post)
-      console.log("post: " + blogs[post]);
 
       function showBlog() {
         $(".blogs-container").html(
             "<div class='blog-item'>"+
-              "<div class='blog-title dark-purple'><span class='inliner inc-md-size-text'>Blog #" + post + ": " + blogs[post].title + "</span></div>"+
+              "<div class='blog-title dark-purple'><span class='inliner inc-md-size-text bolden'>Blog #" + (post+1) + ": " + blogs[post].title + "</span></div>"+
               "<div class='blog-time small-text'>" + blogs[post].time + "</div>"+
               "<div class='blog-body'>" + blogs[post].body + "</div>"+
             "</div>"+
-            "<div class='separator2 dim-blue-bg'></div>"
+            "<div class='separator2 dim-blue-bg'></div>"+
+            "<div id='disqus_thread'></div>"+
+            "<script type='text/javascript'>"+
+          "var disqus_shortname = 'pieceblog';"+
+          "var disqus_url = 'http://piecedigital.github.io/blogpost/blogpost.html?blog=" + post + "';"+
+
+          "(function() {"+
+            "var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;"+
+            "dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';"+
+            "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);"+
+          "})();"+
+          "</script>"+
+          "<noscript>Please enable JavaScript to view the <a href='https://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript>"
           );
       }
       showBlog();
